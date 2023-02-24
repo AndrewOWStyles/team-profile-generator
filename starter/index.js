8648c9e023e8f04f60262a7f157147f4417e862e
Andrew Styles <andystyles@hotmail.co.uk>
2023-02-24 19:18:12 +0000

    added manager code and ran tests to pass

--- starter/index.js
+++ starter/index.js
@@ -1,9 +1,10 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +14,17 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+let team = [];
+
+startProgram()
+async function startProgram() {
+    
+    team.push(new Manager("Andrew", 1, "test@test.com", "001010"))
+    // team.push(new Engineer("Andrew", 2, "test2@test.com"))
+    // team.push(new Intern("Andrew", 3, "test3@test.com"))
+    // team.push(new Intern("Andrew", 4, "test4@test.com"))
+
+    let htmlDoc = render(team);
+
+    await fs.writeFile(outputPath, htmlDoc);
+}
