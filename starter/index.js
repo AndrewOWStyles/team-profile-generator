e4622e9fe2d8f623e719fedf497f17f3049b7437
Andrew Styles <andystyles@hotmail.co.uk>
2023-02-26 13:26:24 +0000

    manager prompts now write to file when using inquirer

--- starter/index.js
+++ starter/index.js
@@ -11,20 +11,51 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
 let team = [];
 
-startProgram()
 async function startProgram() {
-    
-    team.push(new Manager("Andrew", 1, "test@test.com", "001010"))
-    team.push(new Engineer("Andrew", 2, "test2@test.com", "testgit"))
-    team.push(new Intern("Andrew", 3, "test3@test.com", "testschool1"))
-    team.push(new Intern("Andrew", 4, "test4@test.com", "testschool2"))
-
-    let htmlDoc = render(team);
-
-    await fs.writeFile(outputPath, htmlDoc);
+  await manager();
+  let htmlDoc = render(team);
+  await fs.writeFile(outputPath, htmlDoc);
 }
+
+function manager() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the Manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+      },
+    ])
+    .then((managerAnswers) => {
+      const { name, id, email, officeNumber } = managerAnswers;
+      const manager = new Manager(name, id, email, officeNumber);
+      team.push(manager);
+    });
+}
+
+
+
+startProgram();
+
+
+// team.push(new Manager("Andrew", 1, "test@test.com", "001010"))
+// team.push(new Engineer("Andrew", 2, "test2@test.com", "testgit"))
+// team.push(new Intern("Andrew", 3, "test3@test.com", "testschool1"))
+// team.push(new Intern("Andrew", 4, "test4@test.com", "testschool2"))
