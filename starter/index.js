4322570e32927051b6c1230e2cbcee99464b216b
Andrew Styles <andystyles@hotmail.co.uk>
2023-02-26 13:55:25 +0000

    added functions for manager, engineer and intern with inquirer prompts allowing for reusability. habe an addemployee confirm function to allow as many employees as possible and a function to choose the type of employee.

--- starter/index.js
+++ starter/index.js
@@ -15,6 +15,16 @@ let team = [];
 
 async function startProgram() {
   await manager();
+  let addAnotherEmployee = await addAnEmployeeConfirm();
+  while (addAnotherEmployee) {
+    let employeeType = await employeeOptions();
+    if (employeeType === "Engineer") {
+      await engineer();
+    } else {
+      await intern();
+    }
+    addAnotherEmployee = await addAnEmployeeConfirm();
+  }
   let htmlDoc = render(team);
   await fs.writeFile(outputPath, htmlDoc);
 }
@@ -49,6 +59,98 @@ function manager() {
       team.push(manager);
     });
 }
+
+function addAnEmployeeConfirm() {
+  return inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "addEmployee",
+        message: "Would you like to add a new employee?",
+      },
+    ])
+    .then((answers) => {
+      return answers.addEmployee;
+    });
+}
+
+function employeeOptions() {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeType",
+        message: "What type of employee would you like to add?",
+        choices: ["Engineer", "Intern"],
+      },
+    ])
+    .then((answers) => {
+      return answers.employeeType;
+    });
+}
+
+function engineer() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the Engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+      },
+    ])
+    .then((engineerAnswers) => {
+      const { name, id, email, github } = engineerAnswers;
+      const engineer = new Engineer(name, id, email, github);
+      team.push(engineer);
+    });
+}
+
+function intern() {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the Intern's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Employee ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email?",
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "What is your school's name?",
+        },
+      ])
+      .then((internAnswers) => {
+        const { name, id, email, school } = internAnswers;
+        const intern = new Intern(name, id, email, school);
+        team.push(intern);
+      });
+  }
+  
 
 
 
